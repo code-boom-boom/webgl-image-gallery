d3b0503941d3b9bde9db7c68c5595e3205662ef5
Love Coding <fedde198212@gmail.com>
2021-10-07 02:27:15 -0400

    media class finished

--- app/Media.js
+++ app/Media.js
@@ -6,6 +6,8 @@ import { Mesh, Program, Texture } from "ogl";
 import fragment from "shaders/image-fragment.glsl";
 import vertex from "shaders/image-vertex.glsl";
 import Number from "./Number";
+import Title from "./Title";
+import { map } from "./utils/math";
 
 export default class {
     constructor({ geometry, gl, image, index, length, renderer, scene, screen, text, viewport }) {
@@ -25,6 +27,10 @@ export default class {
         this.createShader();
 
         this.createMesh();
+
+        this.createTitle();
+
+        this.onResize();
     }
 
     createShader() {
@@ -76,5 +82,72 @@ export default class {
             renderer: this.renderer,
             text: this.index % (this.length / 2)
         });
+
+        this.title = new Title({
+            gl: this.gl,
+            plane: this.plane,
+            renderer: this.renderer,
+            text: this.text
+        });
+    }
+
+    update(scroll, direction) {
+        this.plane.position.x = this.x - scroll.current - this.extra;
+        this.plane.position.y = Math.cos((this.plane.position.x / this.widthTotal) * Math.PI) * 75 - 74.5;
+        this.plane.rotation.z = map(this.plane.position.x, - this.widthTotal, this.widthTotal, Math.PI, - Math.PI);
+
+        this.speed = scroll.current - scroll.last;
+
+        this.program.uniforms.uTime.value += 0.04;
+        this.program.uniforms.uSpeed.value = this.speed;
+
+        const planeOffset = this.plane.scale.x / 2;
+        const viewportOffset = this.viewport.width;
+
+        this.isBefore = this.plane.position.x + planeOffset < - viewportOffset;
+        this.isAfter = this.plane.position.x - planeOffset < viewportOffset;
+
+        if (direction === "right" && this.isBefore) {
+            this.extra -= this.widthTotal;
+
+            this.isBefore = false;
+            this.isAfter = false;
+        }
+
+        if (direction === "left" && this.isAfter) {
+            this.extra += this.widthTotal;
+
+            this.isBefore = false;
+            this.isAfter = false;
+        }
+    }
+
+    /**
+     * Events.
+     */
+    onResize ({ screen, viewport } = {}) {
+        if (screen) {
+            this.screen = screen;
+        }
+
+        if (viewport) {
+            this.viewport = viewport;
+
+            this.plane.program.uniforms.uViewportSizes.value = [this.viewport.width, this.viewport.height];
+        }
+
+        this.scale = this.screen.height / 1500;
+
+        this.plane.scale.y = this.viewport.height * (900 * this.scale) / this.screen.height;
+        this.plane.scale.x = this.viewport.width * (700 * this.scale) / this.screen.width;
+
+        this.plane.program.uniforms.uPlaneSizes.value = [this.plane.scale.x, this.plane.scale.y];
+
+        this.padding = 2;
+
+        this.width = this.plane.scale.x + this.padding;
+        this.widthTotal = this.width * this.length;
+
+        this.x = this.width * this.index;
     }
 }
